5d0753e27ded792db1a8dd056cb029db5483fa2d
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-25 17:02:25 +0800

    day2, concatenate hello world

---
+++ cSharp/day2.cs
@@ -0,0 +1,10 @@
+// day 2
+// declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
+
+// syntax type variableName = value;
+
+string a = "hello";
+string b = "world";
+
+Console.WriteLine(a + "" +  b);
+
