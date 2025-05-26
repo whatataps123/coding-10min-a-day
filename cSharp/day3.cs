a1fe5bc665df171925f071ca9e23dbdd8ffb31e1
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-26 11:01:27 +0800

    day3, trim whitespace using Trim()

---
+++ cSharp/day3.cs
@@ -0,0 +1,11 @@
+// day 3
+// Trim whitespace from strings using built in function
+
+// syntax type variableName = value;
+
+string a = "      hello      ";
+
+Console.WriteLine(a + " world");
+
+Console.WriteLine(a.Trim() + " world");
+
