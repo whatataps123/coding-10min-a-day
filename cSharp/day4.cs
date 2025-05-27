4a64971913e0fbc3be2475c4301b021ddb2dfb8a
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-27 10:30:02 +0800

    day4, uppercase and lowecase string function

---
+++ cSharp/day4.cs
@@ -0,0 +1,11 @@
+// day 4
+// uppercase-all and lowercase-all strings using built in function
+
+// syntax type variableName = value;
+
+string a = "heLLo";
+
+Console.WriteLine(a.ToUpper()); // output HELLO
+
+Console.WriteLine(a.ToLower()); // output hello
+
