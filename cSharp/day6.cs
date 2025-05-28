55dbe564ec2a1263d9e8a51a1b0ca71dc85e640a
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-29 06:58:43 +0800

    day6, concatenate int and str

---
+++ cSharp/day6.cs
@@ -0,0 +1,11 @@
+// day 6
+//concatenate integer and string without getting an error
+
+string word = "hello ";
+int number = 42;
+
+// Concatenate using the + operator
+
+string result = word + number;
+
+Console.WriteLine(result); // Output: hello42
