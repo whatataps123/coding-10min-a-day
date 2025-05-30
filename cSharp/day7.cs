416b616d1bb982e1797eb0cfc91bf968849f306c
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-30 09:24:29 +0800

    day7, concatenate float and string

---
+++ cSharp/day7.cs
@@ -0,0 +1,7 @@
+// day 7
+// concatenate float and string without getting an error
+
+string text = "The value of pi is ";
+float num = 3.14F;
+
+Console.WriteLine(text + num); // Output: The value of pi is 3.14
