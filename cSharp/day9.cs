eab12683b693be0d4f38ca20737414a760a46b80
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-30 09:34:08 +0800

    day9, array and comma separated

---
+++ cSharp/day9.cs
@@ -0,0 +1,5 @@
+// day 9
+//convert array to a comma separated string (array join)
+
+String []animals = { "cat", "dog", "bird" };
+Console.WriteLine(string.Join(", ", animals)); // Output: cat, dog, bird
