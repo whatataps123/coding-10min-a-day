7dafc34de41205687b1885acac21d8fa861a026e
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-30 09:25:53 +0800

    day8, concatenate bool and str

---
+++ cSharp/day8.cs
@@ -0,0 +1,7 @@
+// day 8
+// contatenate boolean and string without getting an error
+
+bool isTrue = true;
+string message = "The statement is: ";
+Console.WriteLine(message + isTrue); // Output: The statement is: True
+// Concatenate boolean and string without getting an error
