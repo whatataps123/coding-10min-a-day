13ba196bd2f2b886cef1ebd7cddc70600a68b87c
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-06-02 15:45:02 +0800

    day11, split string array single char

---
+++ cSharp/day11.cs
@@ -0,0 +1,10 @@
+// day 11
+// 11. split string to an array of (single) characters
+
+string animals = "single";
+char[] splitWord = animals.ToCharArray();
+Console.WriteLine("Original string: " + animals); // Output: Original string: single
+
+Console.WriteLine(splitWord[0]);
+Console.WriteLine(splitWord[1]);
+Console.WriteLine(splitWord[2]);
