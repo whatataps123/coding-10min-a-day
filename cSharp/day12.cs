89bb93cd38085411b247948b8412ebb3a2cf20e3
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-06-03 07:11:51 +0800

    day12, loop thru numeric array w/o using manual i++

---
+++ cSharp/day12.cs
@@ -0,0 +1,8 @@
+// day 12
+// 12. loop through a numeric array preferrably not using manual index increment
+
+string[] numbers = ["one", "two", "three", "four", "five"];
+foreach (string number in numbers)
+{
+    Console.WriteLine(number);
+}
