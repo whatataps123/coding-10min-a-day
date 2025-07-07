4a7811b93fdc5d1f8172bb7acbb309fd01acdfc6
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-07-07 08:38:56 +0800

    day25, creating function and return sum

---
+++ cSharp/day25.cs
@@ -0,0 +1,10 @@
+// 25. Create function: add(x,y) that returns the sum of 2 numbers
+
+static void add(int x, int y)
+{
+    int sum = x + y; // Calculate the sum of x and y
+    Console.WriteLine($"The sum of {x} and {y} is: {sum}"); // Output the result
+}
+    add(5, 10); // 15
+    add(20, 30); // 50
+    add(-5, 5); // 0
