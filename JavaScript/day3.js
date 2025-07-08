a20b462dbcf80b2d4ce1d3e937a8941149052953
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-07-08 10:03:41 +0800

    js: day 3, trim whitespace

---
+++ JavaScript/day3.js
@@ -0,0 +1,6 @@
+// 3. Trim whitespace from strings using built in function
+
+let a = "  hello world  "
+let b = a.trim()
+console.log(a) //  hello world  
+console.log(b + " people") //hello world people
