6c9fabc99a9e9a5aea9d2342cb26f77936450a9e
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-07-08 09:51:41 +0800

    js: day1, print hello world

---
+++ JavaScript/day1.js
@@ -0,0 +1,8 @@
+// 1. setup environment/compiler and print out hello world
+// download node.js from https://nodejs.org/
+// 2. create a file named day1.js
+// 3. write the following code in day1.js
+
+console.log("Hello, World!");
+
+// node day1.js to run this file in the terminal
