ed600bebd864ff7d178e0f34485c2e9a58ca9d19
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-07-12 21:24:49 +0800

    js: day 6 concatenante int and str

---
+++ JavaScript/day6.js
@@ -0,0 +1,8 @@
+// 6. concatenate integer and string without getting an error
+
+const desc = "I am Lemuel and I'm "
+const num = 21
+
+console.log(desc + num)
+// more explicit
+console.log(desc + num.toString())
