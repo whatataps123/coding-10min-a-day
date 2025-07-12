8874e15234785df67e550cf158e79ab3b915435e
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-07-12 23:36:17 +0800

    js: day 26, implement functions for subtraction, division, and multiplication

---
+++ cSharp/day26.cs
@@ -0,0 +1,22 @@
+// 26. Create functions: subtract(x,y), divide(x,y), multiple(x,y)
+
+static void subtract(int x, int y)
+{
+    int diff = x - y;
+    Console.WriteLine($"The difference of {x}-{y} is {diff}");
+}
+static void divide(int x, int y)
+{
+    int quotient = x / y;
+    Console.WriteLine($"The quotient of {x}/{y} is {quotient}");
+}
+
+static void multiply(int x, int y)
+{
+    int product = x * y;
+    Console.WriteLine($"The product of {x}*{y} is {product}");
+}
+
+subtract(10, 5);
+divide(10, 5);
+multiply(10, 5);
